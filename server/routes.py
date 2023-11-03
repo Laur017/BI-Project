5f1f540f126b21f83b4a98304b22e86d5fc039d2
daniel-bicu <daniell.bicu008@gmail.com>
2023-11-03 20:59:17 +0200

    Add country sales associations based on media_types

--- server/routes.py
+++ server/routes.py
@@ -1,9 +1,19 @@
 # routers/todos.py
 from fastapi import APIRouter
 from setup import postgres_conn
-
+import pycountry
 
 router = APIRouter()
+
+
+def get_alpha_3_country_code(country_name):
+    try:
+        country = pycountry.countries.search_fuzzy(country_name)[0]
+        return country.alpha_3
+    except LookupError:
+        return None
+    
+    
 
 @router.get("/", response_model=list)
 async def get_all_customers():
@@ -50,13 +60,37 @@ async def get_customer_music_associations_data(lower_age: int=20, upper_age: int
         
     return data
     
-    
-    
-    
 # 1.c.
 @router.get("/country-musictypes-sales")
 async def get_country_music_types_sales_data():
-    pass
+    data = []
+    
+    media_types_data = {
+        
+    }
+    
+    try:
+        QUERY = f"SELECT * FROM COUNTRY_MEDIATYPE_SALES"
+        cursor = postgres_conn.cursor()
+        cursor.execute(query=QUERY)
+        
+        data = cursor.fetchall()
+        for record in data: 
+            if record[1] in media_types_data:
+                media_types_data[record[1]]['media_type'] = record[1]
+                media_types_data[record[1]]['countries'].append(get_alpha_3_country_code(record[0]))
+                media_types_data[record[1]]['sales'].append(record[2])
+            else: 
+                media_types_data[record[1]] = dict()
+                media_types_data[record[1]]['countries']=[record[0]]
+                media_types_data[record[1]]['sales']=[record[2]]
+             
+    except Exception as e:
+        print('error, details: ' + str(e))
+    finally:
+        cursor.close()
+        
+    return [value for key, value in media_types_data.items()]
 
 
 # -----  serve point 2 of HW1
