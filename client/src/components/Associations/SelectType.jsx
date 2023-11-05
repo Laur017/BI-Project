85ccc496e6d073c4d793718f8ff35c2449af9bd9
Laurentiu Popa <popa.laur.01@gmail.com>
2023-11-05 18:32:14 +0200

    Update SelectType.jsx

--- client/src/components/Associations/SelectType.jsx
+++ client/src/components/Associations/SelectType.jsx
@@ -9,24 +9,58 @@ export default function SelectType() {
     const [percentages, setPercentages] = useState();
     const [salesValue, setSalesValue] = useState();
     const [maxValue, setMaxValue] = useState();
-    const n = sales.length;
-    const countries = sales[0].country;
+    const [data, setData] = useState([]);
+    const [mediaT, setMediaT] = useState([]);
+    const [countries, setCountries] = useState([])
+    const [options, setOptions] = useState()
+
+    let n;
+    // const countries = sales[0].country;
     let bulletSize = new Array(countries.length).fill(30);   
 
-    let mediaType = [];
-    for(let i of sales){
-        mediaType.push(i.media_type)
-    }
+    let mediaType;
 
-    const Options = mediaType.map((i,indx) =>
-    <option key={indx} value={i} onClick={() => setSelectedType(i)}>{i}</option>
-    )
 
     useEffect(()=>{
 
-      for(let i of sales){
+      fetch('http://127.0.0.1:8000/api/v1/music-sales-data/country-musictypes-sales')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+
+    },[showGraph]) 
+
+    let Options;
+
+  useEffect(() => {
+    console.log(data)
+    mediaType = []
+    for(let i of data){
+        mediaType.push(i.media_type)
+    }
+   setMediaT(mediaType)
+
+   
+  },[data])
+
+    useEffect(() => {
+      
+      Options = mediaT.map((i,indx) =>
+      <option key={indx} value={i} onClick={() => setSelectedType(i)}>{i}</option>
+      )
+
+      setOptions(Options)
+
+    },[mediaT])
+
+   
+
+    useEffect(()=>{
+
+      for(let i of data){
         if(i.media_type === selectedType){
             setSalesValue(i.sales)
+            setCountries(i.countries)
         }
     }
 
@@ -47,7 +81,7 @@ export default function SelectType() {
                 <h4>Select media type: </h4>
                 <select className='select-job'>
                     
-                    {Options}
+                    {mediaT && options}
                 </select>
             </div>
 
@@ -75,7 +109,7 @@ export default function SelectType() {
         }]}
         layout = {{
           'geo': {
-              'scope': 'europe',
+              'scope': 'world',
               'bgcolor': 'rgba(0,0,0,0)',
               'showland': true,
               'landcolor': 'white',
@@ -84,7 +118,7 @@ export default function SelectType() {
           plot_bgcolor:"transparent", 
           paper_bgcolor:"rgba(0,0,0,0)",
           width:800, 
-          height:800
+          height:500
         }}
         />
         </div>
