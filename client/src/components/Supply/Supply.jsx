c0736c8527a7da40f1f8ab612a9b6ea6a70afdfb
Laurentiu Popa <popa.laur.01@gmail.com>
2023-11-05 18:45:20 +0200

    Update Supply.jsx

--- client/src/components/Supply/Supply.jsx
+++ client/src/components/Supply/Supply.jsx
@@ -1,25 +1,49 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import transition from '../../transition'
 import Plot from 'react-plotly.js'
 import {media} from '../../data'
 
 function Supply() {
-  const res = media.map(i => (
-    {
-      type:'bar', 
-      x: i.months,
-      y:i.sales,
-      name: i.media_type
-    }
-  ))
+  const [data,setData] = useState([])
+  const [resFinal, setResFinal] = useState()
+
+
+
+  useEffect(()=>{
+
+    fetch('http://127.0.0.1:8000/api/v1/music-sales-data/average-per-mediatype-based-on-past-months')
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error(error));
+
+
+  },[]) 
+
+  useEffect(() => {
+
+    const res = data.map(i => (
+      {
+        type:'bar', 
+        x:i.months,
+        y:i.avg,
+        name: i.media_type
+      }
+    ))
+
+    setResFinal(res)
+
+  },[data])
+
+
+  
   return (
     <div className='div-supply general-div'>
      <h4>Average Supply needed</h4>
      <Plot
         data={
-          res
+          resFinal
         }
         layout = {{
           plot_bgcolor:"transparent", 
